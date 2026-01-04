9d9c74a9c292260db0a5e9be9ca2758b7ffe93d9
A-zeril-A <a.zeril.a@gmail.com>
2026-01-04 17:44:33 +0000

    Remove junk dir

--- isa1/module_from_other_vendor/stranbys_saleorder_revision/models/__init__.py
+++
@@ -1,5 +0,0 @@
-#    Stranbys Info Solution. Ltd.
-#    ===========================
-#    Copyright (C) 2022-TODAY Stranbys Stranbys Info Solution(<https://www.stranbys.com>)
-#    Author: Stranbys Info Solution(<https://www.stranbys.com>)
-from . import sale
